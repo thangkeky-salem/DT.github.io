e7fa6aea2c5428c5d94cae659e7a397ffa55a55f
thangkeky-salem <thangkeky@gmail.com>
2025-08-28 00:32:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 
+$(document).ready(function() {
+    // nav toggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width()<768){
+            $(".header .nav").slideToggle();
+        }
+    })
+    // audio
+    $("#myAudio")[0].play();
+    $(".bi-music-note-beamed").click(function(){
+        if($(this).hasClass("pause")){
+            $("#myAudio")[0].play();
+        }else{
+        $("#myAudio")[0].pause();
+        }
+    $(this).toggleClass("pause");
+    })
+    //fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })
+    //scroll
+        $.scrollIt({
+            topOffset: -50
+        });
+    
+});
+    
